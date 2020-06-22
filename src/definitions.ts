a97b3c39e39b6257e4f1770937b64be2f3a269a4
Priyank Patel <priyank.patel@stackspace.ca>
2020-06-22 09:39:57 -0400

    Add SignInResponse and SignInError type definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -7,7 +7,7 @@ declare module "@capacitor/core" {
 export interface SignInWithApplePlugin {
   Init(options: InitOptions): Promise<void>;
 
-  Authorize(): Promise<{ response: any }>;
+  Authorize(): Promise<SignInResponse>;
 }
 
 export interface InitOptions {
@@ -16,4 +16,29 @@ export interface InitOptions {
   redirectURI: string;
   state: string;
   usePopup: boolean;
+}
+
+export interface SignInResponse {
+  user: User;
+  authorization: Authorization;
+}
+
+export interface SignInError {
+  error: string;
+}
+
+export interface Authorization {
+  code: string;
+  id_token: string;
+  state: string;
+}
+
+export interface User {
+  email: string;
+  name: Name;
+}
+
+export interface Name {
+  firstName: string;
+  lastName: string;
 }
