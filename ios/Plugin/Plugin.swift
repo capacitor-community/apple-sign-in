0afa3da324ae95aa7b2543efb6019cd4514a681d
Jose Martinez <epicshaggy@gmail.com>
2021-07-02 16:47:54 -0400

    bug fixes

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,17 +8,19 @@ import AuthenticationServices
  */
 @objc(SignInWithApple)
 public class SignInWithApple: CAPPlugin {
-    var call: CAPPluginCall?
 
     @objc func authorize(_ call: CAPPluginCall) {
-        self.call = call
-
         if #available(iOS 13.0, *) {
             let appleIDProvider = ASAuthorizationAppleIDProvider()
             let request = appleIDProvider.createRequest()
             request.requestedScopes = getRequestedScopes(from: call)
             request.state = call.getString("state")
             request.nonce = call.getString("nonce")
+            
+            let defaults = UserDefaults()
+            defaults.setValue(call.callbackId, forKey: "callbackId")
+            
+            self.bridge?.saveCall(call)
 
             let authorizationController = ASAuthorizationController(authorizationRequests: [request])
             authorizationController.delegate = self
@@ -53,12 +55,14 @@ public class SignInWithApple: CAPPlugin {
 @available(iOS 13.0, *)
 extension SignInWithApple: ASAuthorizationControllerDelegate {
     public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
-            call?.reject("Please, try again.")
-
+        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
+        
+        let defaults = UserDefaults()
+        let id = defaults.string(forKey: "callbackId") ?? ""
+        guard let call = self.bridge?.savedCall(withID: id) else {
             return
         }
-
+        
         let result = [
             "response": [
                 "user": appleIDCredential.user,
@@ -70,10 +74,17 @@ extension SignInWithApple: ASAuthorizationControllerDelegate {
             ]
         ]
 
-        call?.resolve(result as PluginResultData)
+        call.resolve(result)
+        self.bridge?.releaseCall(call)
     }
 
     public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        call?.reject(error.localizedDescription)
+        let defaults = UserDefaults()
+        let id = defaults.string(forKey: "callbackId") ?? ""
+        guard let call = self.bridge?.savedCall(withID: id) else {
+            return
+        }
+        call.reject(error.localizedDescription)
+        self.bridge?.releaseCall(call)
     }
 }
