0897f8223a813c1bda860a6f5e49fb6dd29f1487
jcesarmobile <jcesarmobile@gmail.com>
2022-09-30 15:51:27 +0200

    refactor(ios): Remove unneeded code

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -2,35 +2,26 @@ import Foundation
 import Capacitor
 import AuthenticationServices
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitor.ionicframework.com/docs/plugins/ios
- */
 @objc(SignInWithApple)
 public class SignInWithApple: CAPPlugin {
 
     @objc func authorize(_ call: CAPPluginCall) {
-        if #available(iOS 13.0, *) {
-            let appleIDProvider = ASAuthorizationAppleIDProvider()
-            let request = appleIDProvider.createRequest()
-            request.requestedScopes = getRequestedScopes(from: call)
-            request.state = call.getString("state")
-            request.nonce = call.getString("nonce")
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = getRequestedScopes(from: call)
+        request.state = call.getString("state")
+        request.nonce = call.getString("nonce")
 
-            let defaults = UserDefaults()
-            defaults.setValue(call.callbackId, forKey: "callbackId")
+        let defaults = UserDefaults()
+        defaults.setValue(call.callbackId, forKey: "callbackId")
 
-            self.bridge?.saveCall(call)
+        self.bridge?.saveCall(call)
 
-            let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-            authorizationController.delegate = self
-            authorizationController.performRequests()
-        } else {
-            call.reject("Sign in with Apple is available on iOS 13.0+ only.")
-        }
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.performRequests()
     }
 
-    @available(iOS 13.0, *)
     func getRequestedScopes(from call: CAPPluginCall) -> [ASAuthorization.Scope]? {
         var requestedScopes: [ASAuthorization.Scope] = []
 
@@ -52,7 +43,6 @@ public class SignInWithApple: CAPPlugin {
     }
 }
 
-@available(iOS 13.0, *)
 extension SignInWithApple: ASAuthorizationControllerDelegate {
     public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
