6e7fe5b861f866cd85e8044d0d29bba06aa3e51f
Piotr <hello@piotr.cz>
2024-02-08 16:15:30 +0100

    Update to Capacitor v5

--- android/src/main/java/com/getcapacitor/community/applesignin/SignInWithApple.java
+++ android/src/main/java/com/getcapacitor/community/applesignin/SignInWithApple.java
@@ -1,20 +1,20 @@
 package com.getcapacitor.community.applesignin;
 
 import com.getcapacitor.JSObject;
-import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
 
-@NativePlugin
+@CapacitorPlugin(name = "Echo")
 public class SignInWithApple extends Plugin {
 
-    @PluginMethod
+    @PluginMethod()
     public void echo(PluginCall call) {
         String value = call.getString("value");
 
         JSObject ret = new JSObject();
         ret.put("value", value);
-        call.success(ret);
+        call.resolve(ret);
     }
 }
